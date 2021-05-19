96f2877dcd0edda3a94db12512b2a96f57860967
Ekaterina Ledeneva <ekiuled@gmail.com>
2021-05-19 17:08:00 +0300

    Scene update

--- ogre/src/model/entities/Light.cpp
+++ ogre/src/model/entities/Light.cpp
@@ -5,7 +5,7 @@ Light::Light(View& view) : Entity(view), light(view.createLight()) {
     light->setDiffuseColour(0.7, 0.5, 0.6);
     light->setSpecularColour(1.0, 0, 1.0);
 
-    sceneNode->setPosition(10, 30, 20);
-    sceneNode->setDirection(-10, -30, -20);
+    sceneNode->setPosition(10, 60, 50);
+    sceneNode->setDirection(-10, -60, -50);
     sceneNode->attachObject(light);
 }
